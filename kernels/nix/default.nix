985a4ba1470b3fb237bf5b14e77b7b2cae7736ed
Daniel Baker <daniel.baker@tweag.io>
2022-05-31 15:57:49 -0700

    Working state for trying to get depedencies working properly.

--- kernels/nix/default.nix
+++ kernels/nix/default.nix
@@ -3,40 +3,80 @@
   pkgs,
   poetry2nix,
   nix ? pkgs.nixVersions.stable,
+  # https://github.com/nix-community/poetry2nix#mkPoetryPackages
   projectDir ? null,
   pyproject ? null,
   poetrylock ? null,
+  overrides ? null,
   python ? null,
-}:
-let
-  env = poetry2nix.mkPoetryPackages
-  {
-    projectDir =
-      if projectDir == null
-      then self + "/kernels/nix"
-      else projectDir;
+  editablePackageSources ? {},
+}: let
+  inherit (pkgs) lib;
 
-    pyproject =
-      if pyproject == null
-      then projectDir + "/pyproject.toml"
-      else pyproject;
+  addNativeBuildInputs = drv: inputs:
+    drv.overridePythonAttrs (old: {
+      nativeBuildInputs = (old.nativeBuildInputs or []) ++ inputs;
+    });
 
-    poetrylock =
-      if poetrylock == null
-      then projectDir + "/poetry.lock"
-      else poetrylock;
+  _projectDir =
+    if projectDir == null
+    then self + "/kernels/nix"
+    else projectDir;
 
-    python =
-      if python == null
-      then pkgs.python3
-      else python;
-  };
+  env =
+    poetry2nix.mkPoetryApplication
+    {
+      projectDir = _projectDir;
 
-  nix-bin = pkgs.writeScriptBin "nix-kernel"
+      pyproject =
+        if pyproject == null
+        then _projectDir + "/pyproject.toml"
+        else pyproject;
+
+      poetrylock =
+        if poetrylock == null
+        then _projectDir + "/poetry.lock"
+        else poetrylock;
+
+      python =
+        if python == null
+        then pkgs.python3
+        else python;
+
+      overrides =
+        if overrides == null
+        then
+          poetry2nix.overrides.withDefaults (self: super: {
+            traitlets = addNativeBuildInputs super.traitlets [self.hatchling];
+            terminado = addNativeBuildInputs super.terminado [self.hatchling];
+            ipykernel = super.ipykernel.overridePythonAttrs (old: {
+              postPatch = ''
+                sed -i "/debugpy/d" setup.py
+              '';
+            });
+          })
+        else overrides;
+    };
+
+  # Transform python3.9-xxxx-1.8.0 to xxxx
+  toName = s:
+    lib.strings.concatStringsSep "-"
+    (lib.lists.drop 1 (lib.lists.init (lib.strings.splitString "-" s)));
+
+  # Makes the flat list an attrset
+  packages = builtins.foldl' (obj: drv: {"${toName drv.name}" = drv;} // obj) {} env.poetryPackages;
+
+  nix-bin =
+    pkgs.runCommand "wrapper-${env.name}"
+    {nativeBuildInputs = [pkgs.makeWrapper];}
     ''
-      #! ${pkgs.stdenv.shell}
-      PATH=${nix}/bin/:${env.python}/bin:$PATH
-      exec python -m nix-kernel $@
+      mkdir -p $out/bin
+      for i in ${env}/bin/*; do
+        filename=$(basename $i)
+        ln -s ${env}/bin/$filename $out/bin/$filename
+        wrapProgram $out/bin/$filename \
+          --set PATH ${nix}/bin
+      done
     '';
 in
   {
@@ -50,14 +90,13 @@ in
     ],
     codemirror_mode ? "",
     logo64 ? ./logo64.png,
-  }:
-    {
-      inherit
-        name
-        displayName
-        language
-        argv
-        codemirror_mode
-        logo64
-        ;
-    }
+  }: {
+    inherit
+      name
+      displayName
+      language
+      argv
+      codemirror_mode
+      logo64
+      ;
+  }
