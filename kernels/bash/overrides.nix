8e5125ef297a593c06d2da0643168e758d855225
Rok Garbas <rok@garbas.si>
2022-06-20 22:58:54 +0200

    fix last bits of bash-kernel

--- kernels/bash/overrides.nix
+++ kernels/bash/overrides.nix
@@ -5,9 +5,17 @@ final: prev: let
     });
   };
 in
-  {}
-  // addNativeBuildInputs "bash-kernel" [final.flit-core]
-  // addNativeBuildInputs "bash-kernel" [final.ipykernel]
+  {
+    bash-kernel = prev.bash-kernel.overridePythonAttrs (old: {
+      nativeBuildInputs = (old.nativeBuildInputs or []) ++ [final.flit-core final.ipykernel];
+      postPatch = ''
+        sed -i \
+          -e 's|requires = \["flit"\]|requires = \["flit_core >=3.2,<4"\]|' \
+          -e 's|build-backend = "flit.buildapi"|build-backend = "flit_core.buildapi"|' \
+            pyproject.toml
+      '';
+    });
+  }
   // addNativeBuildInputs "traitlets" [final.hatchling]
   // addNativeBuildInputs "jupyter-client" [final.hatchling]
   // addNativeBuildInputs "ipykernel" [final.hatchling]
