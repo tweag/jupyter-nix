a18ce92803fd575b2c7715f366c9f89affef38cf
Rok Garbas <rok@garbas.si>
2022-06-22 11:37:17 +0200

    remove nmot needed stuff

--- kernels/rust/overrides.nix
+++
@@ -1,7 +0,0 @@
-final: prev: let
-  addNativeBuildInputs = drvName: inputs: {
-    "${drvName}" = prev.${drvName}.overridePythonAttrs (old: {
-      nativeBuildInputs = (old.nativeBuildInputs or []) ++ inputs;
-    });
-  };
-in {}
